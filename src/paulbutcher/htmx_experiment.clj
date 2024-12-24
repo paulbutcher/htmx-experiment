2c586c96cf7378331047415f9e3adb68a54b35fe
Paul Butcher <paul@paulbutcher.com>
2024-12-24 16:44:45 +0000

    Skeleton HTMX app

--- src/paulbutcher/htmx_experiment.clj
+++ src/paulbutcher/htmx_experiment.clj
@@ -1,12 +1,34 @@
 (ns paulbutcher.htmx-experiment
-  (:gen-class))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :as route]
+            [hiccup.core :refer [html]]
+            [hiccup.page :refer [html5]]))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(defn index-page []
+  (html5
+   [:head
+    [:title "HTMX Example"]
+    [:script {:src "https://unpkg.com/htmx.org@1.9.6"}]]
+   [:body
+    [:h1 "HTMX Example"]
+    [:div#greeting
+     {:hx-get "/greet"
+      :hx-trigger "load"}]]))
+
+(defn greet []
+  (html [:div "Hello, World!"]))
+
+(defroutes app-routes
+  (GET "/" [] (index-page))
+  (GET "/greet" [] (greet))
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+  [& _args]
+  (jetty/run-jetty app {:port 3000 :join? false})
+  (println "Server started on port 3000"))
